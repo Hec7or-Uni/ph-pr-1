4631da3705af11217e3f5be7e8d94df2520bc80b
hector toral <hectortoraltrabajos@gmail.com>
2022-09-20 09:42:29 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "conecta4_2022.h"
+#include "entrada.h"
+
+// MAIN 
+int main (void) {
+	//inicializar sistema
+	//... practica 2
+	
+	//jugar
+	conecta4_jugar();
+	
+	while(1); //no hay S.O., no se retorna
+}
